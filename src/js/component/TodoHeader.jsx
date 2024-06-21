5497c8194b27f74768aa02c794c54e9f4d8e3930
AliRahimi123456 <162766248+AliRahimi123456@users.noreply.github.com>
2024-06-21 15:00:13 +0000

    the todoList is completed

--- src/js/component/TodoHeader.jsx
+++ src/js/component/TodoHeader.jsx
@@ -9,43 +9,34 @@ const [newTask, setNewTask] = useState("");
 const [idCounter, setIdCounter] = useState(0)
 
 
-const addTask = () => {
-	// console.log("Creating new task: ", newTask);
-
+const addTask = (e) => {
 	let newTodoObject = {
 		id: idCounter,
 		title: newTask,
+
 	}
-//the ... todos, the dots are what we call the spread operator
-//The spread operator expands the array into its elements
-//and then newTodoObject is added at the end of the array.
-//it is a way to push into an array.
+if (e.key == "Enter" && newTask != "") {
+
 	setTodos([...todos, newTodoObject]);
 	setIdCounter(idCounter + 1);
+	setNewTask("")
+	
+}
+
 	
 
 }
-//text validation
-const checkTextBox = () => {
-let textBox = document.querySelector(".task-input");
-	if (textBox.value === "") {
-		alert("Please add a task.")
-	} else {
-		addTask();
-		setNewTask("");
-	}
 
-
-} 
+ 
 	return (
 	<>
 	<header className="header">
 <h1>todos</h1>
 <input className="task-input" type="text"
  placeholder="What needs to be done?"
-  value={newTask} onChange={event => setNewTask(event.target.value)}/>
+  value={newTask} onChange={event => setNewTask(event.target.value)} onKeyDown = {(e) => addTask(e)}/>
 
-<button onClick={checkTextBox}>Add Task</button>
+
     </header>
 	</>
 	);
